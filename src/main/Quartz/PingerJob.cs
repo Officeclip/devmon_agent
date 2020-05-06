056746a6f73e8e45271db956bc3cee55908f650a
SK Dutta <skd@officeclip.co>
2020-05-06 13:54:43 -0400

    added he quartz package

---
+++ src/main/Quartz/PingerJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geheb.DevMon.Agent.Quartz
+{
+    /// <summary>
+    /// Sends ping to the server every minute
+    /// </summary>
+    public class PingerJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+
+        {
+            await Console.Out.WriteLineAsync("PingerJob is executing.");
+        }
+    }
+}

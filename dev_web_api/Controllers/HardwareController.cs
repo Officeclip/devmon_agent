6f04bf08cc534fd9ac825ad9bd9e480aeac866ab
SK Dutta <skd@officeclip.com>
2020-05-31 23:08:41 -0400

    All the controllers have been written... now we will have to plan and test it

---
+++ dev_web_api/Controllers/HardwareController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace dev_web_api.Controllers
+{
+    public class HardwareController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Post(HttpRequestMessage req)
+        {
+            var data = req.Content.ReadAsStringAsync().Result;
+            return Ok();
+        }
+    }
+}

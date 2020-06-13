c1cf0eade7d0a9af90c3aa95624ed37274f0b601
SK Dutta <skd@officeclip.com>
2020-06-12 22:06:30 -0400

    added the service... not tested

--- devmon_service/Service1.cs
+++ devmon_service/Service1.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using devmon_library.Quartz;
+using Newtonsoft.Json.Linq;
+using Quartz;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -12,17 +16,41 @@ namespace devmon_service
 {
     public partial class Service1 : ServiceBase
     {
+        IScheduler scheduler;
         public Service1()
         {
             InitializeComponent();
+            AddAgentGuid();
         }
 
         protected override void OnStart(string[] args)
         {
+            System.Diagnostics.Trace.WriteLine(Directory.GetCurrentDirectory());
+            scheduler = JobScheduler
+                                .Start()
+                                .ConfigureAwait(false)
+                                .GetAwaiter()
+                                .GetResult();
         }
 
         protected override void OnStop()
         {
+            JobScheduler.Stop(scheduler);
+        }
+
+        private void AddAgentGuid()
+        {
+            var appFolder = AppDomain.CurrentDomain.BaseDirectory;
+            JObject settings = JObject.Parse(
+                                        File.ReadAllText(
+                                                $"{appFolder}appSettings.json"));
+            if ((string)settings["agent_guid"] == "")
+            {
+                settings["agent_guid"] = Guid.NewGuid().ToString();
+            }
+            File.WriteAllText(
+                $"{appFolder}appSettings.json", 
+                settings.ToString());
         }
     }
 }

18e1fd3326393e91f9c93b90edb6bb8ea1b71419
SK Dutta <skd@officeclip.com>
2020-08-02 08:55:55 -0400

    added code to recreate the server in monitor.officeclip.com

---
+++ dev_web_api/sql/serverStatic.sql
@@ -0,0 +1,16 @@
+﻿-- Code to created db in monitor.officeclip.com
+BEGIN TRANSACTION
+
+INSERT INTO 
+    "groups" 
+    ("id","server_guid") 
+VALUES 
+    (1,'ad137570-68c8-481c-ad22-f96e3cf41ea5');
+
+INSERT INTO 
+    "users" 
+    ("user_id","email_address", "password", "email_opt") 
+VALUES 
+    (1,'skd@officeclip.com', 'abcd1234@A', 1);
+
+COMMIT
